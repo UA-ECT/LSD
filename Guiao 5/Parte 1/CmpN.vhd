79e823f13c855cbff328052b768b23b1ce666549
Guilherme Alves <36820966+Guilheda@users.noreply.github.com>
2021-04-29 10:50:14 +0100

    Guiao 5

--- Guiao 5/Parte 1/CmpN.vhd
+++ Guiao 5/Parte 1/CmpN.vhd
@@ -1,7 +1,7 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.all;
 use IEEE.NUMERIC_STD.all;
-entity CmpN is
+entity CmpN is
 	generic(N : positive := 16);
 	port(	input0     : in  std_logic_vector(N - 1 downto 0);
 			input1     : in  std_logic_vector(N - 1 downto 0);
